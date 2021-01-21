26152192f765c7d6a84f6d0ef3a15076cb85d2f7
tlecheva <thomas.lechevalier@menaps.com>
2021-01-21 10:44:53 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+
+class HelloMessage extends React.Component {
+  render() {  // afficher moi, se déclenche si changement de props, ou de state/état interne des data du composant
+    return (  // return du JSX
+      <div>
+        Salut {this.props.name} {this.props.famille}
+      </div>
+    );
+  }
+}
+
 
 function App() {
   return (
@@ -17,6 +30,9 @@ function App() {
         >
           Learn React
         </a>
+        <br/>
+        <HelloMessage name="Thomas" />
+
       </header>
     </div>
   );
